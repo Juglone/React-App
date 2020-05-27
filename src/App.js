b54d4009ea40ac09b50af35cb26eaebaddbf7ca6
Julia <elexa.elliot@gmail.com>
2020-05-28 01:59:35 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './App.css';
+import hund_bild from './doggo2.png'
+import polar_bear from './bjorn.gif'
+import pratbubbla from './pratbubbla1.png'
+import Typical from 'react-typical';
+
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Välkommen till min sida! :-)</h1>
+        <br></br>
+        <div>
+          <div class="division"><img src={pratbubbla} className="dialog" alt="En Pratbubbla" /></div>
+          <div class="division"><img src={hund_bild} className="Doggo" alt="En söt Hund" /></div>
+          <div class="division"><img src={polar_bear} className="Bjorn" alt="En vandrande Isbjörn" /></div>
+        </div>
+
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h3>Klicka för att få höra min favoritlåt!!</h3>
+        </a>
+        <br></br>
+        <p>
+          Tjena{' '}
+          <Typical
+          steps={[
+            'Henric', 
+            1000, 
+            'Wiktor!', 
+            1000
+          ]}
+          loop={Infinity}
+          wrapper="b"
+        />
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+
+export default Home;
+
+
